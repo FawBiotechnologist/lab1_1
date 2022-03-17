b3e4c7bba0198a7c045c38bc2b86bac3fb3fc783
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-17 15:39:01 +0100

    added hashCode and equals methods in Money

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -11,6 +11,24 @@ public class Money {
         this.currency = currency;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        Money money = (Money) o;
+        return getAmount().equals(money.getAmount()) && getCurrency().equals(money.getCurrency()) ||
+                (getAmount().equals(new BigDecimal(0)) && money.getAmount().equals(new BigDecimal(0)));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (amount == null ? 0 : amount.hashCode());
+        result = prime * result + (currency == null ? 0 : currency.hashCode());
+        return result;
+    }
+
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
     }
